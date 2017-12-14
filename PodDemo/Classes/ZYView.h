cf459c35b1fa9fb4ed413a88dc73dfb0bbbd1663
zhangya <ya.zhang@weimob.com>
2017-12-14 16:31:09 +0800

    测试页面

---
+++ PodDemo/Classes/ZYView.h
@@ -0,0 +1,12 @@
+//
+//  ZYView.h
+//  AFNetworking
+//
+//  Created by zhangya on 2017/12/14.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZYView : UIView
+
+@end
---
+++ PodDemo/Classes/ZYView.m
@@ -0,0 +1,20 @@
+//
+//  ZYView.m
+//  AFNetworking
+//
+//  Created by zhangya on 2017/12/14.
+//
+
+#import "ZYView.h"
+
+@implementation ZYView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
